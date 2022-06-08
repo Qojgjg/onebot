9bd8799a83f9e314076c28cf6c7d99bd8451dc2c
jimao <jimao@jimaodeMacBook-Pro.local>
2022-06-08 16:13:15 +0800

    .gitignore

--- index.ts
+++ index.ts
@@ -1,6 +1,21 @@
+import ccxt from 'ccxt';
 import { MartinRobot } from './martin';
 
 console.log('你好，世界');
 
-const robot = new MartinRobot();
-robot.Long(0.001);
+async function main() {
+  const binance = new ccxt.binance({
+    // apiKey: Secret.API_KEY,
+    // secret: Secret.SECRET_KEY,
+    enableRateLimit: true,
+    options: {
+      defaultType: 'future',
+    },
+  });
+
+
+  const robot = new MartinRobot();
+  robot.Long(0.001);
+}
+
+main();
